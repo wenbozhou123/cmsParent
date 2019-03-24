ea11fe7e2cee1792101ba61a30df57ea1ffe2b77
Bo-Wen Zhou <bo-wen.zhou@microfocus.com>
2019-03-24 17:30:32 +0800

    solved test case fail

--- basic-hibernate/src/test/java/com/bowen/basic/dao/TestUserDao.java
+++ basic-hibernate/src/test/java/com/bowen/basic/dao/TestUserDao.java
@@ -8,14 +8,18 @@ import org.dbunit.DatabaseUnitException;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.operation.DatabaseOperation;
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.orm.hibernate5.SessionHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.inject.Inject;
 import java.io.FileNotFoundException;
@@ -30,40 +34,48 @@ public class TestUserDao extends AbstractDbUnitTestCase{
     @Inject
     private IUserDao userDao;
 
+    @Inject
+    private SessionFactory sessionFactory;
+
     @Before
     public void setUp() throws SQLException, IOException, DataSetException {
+        Session s = sessionFactory.openSession();
+        TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(s));
         this.backupAllTable();
     }
 
-  /*  @Test
+    @Test
     public void testLoad() throws DatabaseUnitException, SQLException {
         IDataSet ds = createDataSet("t_user");
         DatabaseOperation.CLEAN_INSERT.execute(dbunitCon ,ds);
-        User u = userDao.load(2);
+        User u = userDao.load(1);
         EntitiesHelper.assertUser(u);
-    }*/
-
-    @Test
-    public void testDelete() throws DatabaseUnitException, SQLException {
-        //IDataSet ds = createDataSet("t_user");
-        //DatabaseOperation.CLEAN_INSERT.execute(dbunitCon, ds);
-        User u =new User(10000,"testName10000");
-        userDao.add(u);
-        //User tu= userDao.load(100);
-        //System.out.println(tu.getUsername());
     }
 
-  /*  @Test
+    @Test(expected = ObjectNotFoundException.class)
+    public void testDelete() throws DatabaseUnitException, SQLException {
+        IDataSet ds = createDataSet("t_user");
+        DatabaseOperation.CLEAN_INSERT.execute(dbunitCon, ds);
+        userDao.delete(1);
+        User tu= userDao.load(1);
+        System.out.println(tu.getUsername());
+    }
+
+    @Test
     public void testListByArgs() throws DatabaseUnitException, SQLException {
         IDataSet ds = createDataSet("t_user");
         DatabaseOperation.CLEAN_INSERT.execute(dbunitCon ,ds);
         User u = userDao.load(1);
         EntitiesHelper.assertUser(u);
-    }*/
+    }
 
     @After
     public void tearDown() throws FileNotFoundException, DatabaseUnitException, SQLException {
-        //this.resumeTable();
+        SessionHolder sessionHolder = (SessionHolder) TransactionSynchronizationManager.getResource(sessionFactory);
+        Session s = sessionHolder.getSession();
+        s.flush();
+        TransactionSynchronizationManager.unbindResource(sessionFactory);
+        this.resumeTable();
     }
 
 }
